5a6af477879a2d1553a9a62e7ddacd7e35e41db8
AlecAlmiranez <almiranezalec8@gmail.com>
2024-08-08 11:59:54 +0800

    Added search scope in Idea model

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,16 +13,18 @@ class DashboardController extends Controller
     public function index()
     {
 
-        $ideas = Idea::withCount('likes')->orderBy('created_at', 'DESC');
+        // $ideas = Idea::withCount('likes')->orderBy('created_at', 'DESC');
 
-        if(request()->has('search')){
-            $ideas = $ideas->where('content','like','%'. request()->get('search',''). '%');
-        }
+        // if(request()->has('search')){
+        //     $ideas = $ideas->search(request('search',''));
+        // }
 
+        $ideas = Idea::when(request()->has('search'), function($query){
+            $query->search(request('search',''));
+        })->orderBy('created_at','DESC');
 
         return view('dashboard', [
             'ideas' => $ideas->paginate(5),
-            'topUsers' => $topUsers,
         ]);
     }
 }
