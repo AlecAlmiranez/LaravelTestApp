38109b89dbb4c1f85a1dc094f326106afd567820
AlecAlmiranez <almiranezalec8@gmail.com>
2024-08-10 14:53:20 +0800

    Added Admin widgets

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.layout')
 
-@section('title','Admin Dashboard')
+@section('title', 'Admin Dashboard')
 
 @section('content')
     <div class="container py-4">
@@ -10,6 +10,32 @@
             </div>
             <div class="col-9">
                 <h1> Admin Dashboard </h1>
+
+                <div class = "row">
+                    <div class ="col-sm-6 col-md-4">
+                        @include('shared.widget',[
+                            'title' => 'Total Users',
+                            'icon' => 'fas fa-users',
+                            'data' => $totalUsers,
+                        ])
+                    </div>
+                    <div class ="col-sm-6 col-md-4">
+                        @include('shared.widget',[
+                            'title' => 'Total Ideas',
+                            'icon' => 'fas fa-lightbulb',
+                            'data' => $totalIdeas,
+                        ])
+                    </div>
+                    <div class ="col-sm-6 col-md-4">
+                        @include('shared.widget',[
+                            'title' => 'Total Comments',
+                            'icon' => 'fas fa-comments',
+                            'data' => $totalComments,
+                        ])
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
---
+++ resources/views/shared/widget.blade.php
@@ -0,0 +1,6 @@
+<div class ="card">
+    <div class ="card-body">
+        <p class="fs-3"> <span class ="{{ $icon ?? 'fas fa-users'}}"></span> {{ $title  ?? ''}}</p>
+        <p class="fs-2">{{ $data ?? '0' }}</p>
+    </div>
+</div>
